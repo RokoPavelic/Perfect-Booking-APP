2cae99cb49cf273e2870093099b9be51e490b2ef
RokoPavelic <rokoprivatno@gmail.com>
2022-06-27 14:57:56 +0200

    starter

--- perfectbooking/src/App.js
+++ perfectbooking/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return <h1>Perfect Booking</h1>;
 }
 
 export default App;
